1cd454f059fb371aba1e30050f713b1e58fb0cdf
NasibNia <nasibnia@gmail.com>
2018-08-16 00:19:43 -0700

    moment doesnt add minutes correctly

--- javascript/train.js
+++ javascript/train.js
@@ -11,53 +11,80 @@ firebase.initializeApp(config);
 // Create a variable to reference the database.
 var dataRef = firebase.database();
     
-// Initial Values
+// Defining variables and initializing them
 var name = "";
 var destination = "";
-var firstTrain = 0;
+var firstTrain = "";
 var frequency = "";
+var nextArival = "";
+var minAway = "";
 
-// Capture Button Click
+// click listener for the submit button
 $("#submit").on("click", function(event) {
-  event.preventDefault();
-  
-  // YOUR TASK!!!
-  // Code in the logic for storing and retrieving the most recent user.
-  // Don't forget to provide initial data to your Firebase database.
-  name = $("#train-name").val().trim();
-  destination = $("#destination").val().trim();
-  firstTrain = $("#first-train").val().trim();
-  frequency = $("#frequency").val().trim();
-  
-  // Code for the push
-  dataRef.ref().push({
+    // preventing page refreshing
+    event.preventDefault();
     
-    name: name,
-    destination: destination,
-    firstTrain: firstTrain,
-    frequency: frequency
-    // dateAdded: firebase.database.ServerValue.TIMESTAMP()
-  });
+    // getting the value of input data, trimming the possible additional spaces and the end, and storing them in the predefined variable
+    name = $("#train-name").val().trim();
+    destination = $("#destination").val().trim();
+    firstTrain = $("#first-train").val().trim();
+    frequency = $("#frequency").val().trim();
+  
+    // Pushing the variables in the firebase database
+    dataRef.ref().push({    
+        name: name,
+        destination: destination,
+        firstTrain: firstTrain,
+        frequency: frequency
+    });
 });
-var counter = 0;
-// Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
+
+// Firebase watcher + initial loader ; everytime that a child is added to the database, a snapshot of that child will be taken of the    database, and the function will be executed using that snapshot values.
 dataRef.ref().on("child_added", function(childSnapshot) {
-  counter += 1;
-  console.log("Counter: " + counter);
-  // Log everything that's coming out of snapshot
-  console.log(childSnapshot.val().name);
-  console.log(childSnapshot.val().name);
-  console.log(childSnapshot.val().destination);
-  console.log(childSnapshot.val().firstTrain);
-  console.log(childSnapshot.val().frequency);
+    // var nextArival;
+    // clearInterval(nextArival);
+    // nextArival = setInterval (nextTrainUpdate ,60000);
+    // setInterval(displayUpdate , 60000);   
+
+    var nextArival = nextTrainUpdate (childSnapshot.val().firstTrain , childSnapshot.val().frequency);
+    console.log("nextArival  ", nextArival);
+    var minAway = MinsAwayUpdate (nextArival);
+
+    console.log("freq "  + childSnapshot.val().frequency)
+
+    var newRow = $("<tr>");
+    newRow.append(  "<td>" + childSnapshot.val().name           + "</td>"   +
+                    "<td>" + childSnapshot.val().destination    + "</td>"   +
+                    "<td>" + childSnapshot.val().frequency      + "</td>"   +
+                    "<td>" + childSnapshot.val().nextArival     + "</td>"   +
+                    "<td>" + childSnapshot.val().minAway        + "</td>"   );   
+    $("#data-row").append(newRow);
+    // function nextTrainUpdate(){
+    //     var nextTrain = childSnapshot.val().startTrain;
+    //     var freq = childSnapshot.val().frequency;
+    //     while (moment(nextTrain, "hhmm").isBefore(moment())) {
+    //         nextTrain += freq;    
+    //     }
+    //     return nextTrain;
+    // }
+
+    // function displayUpdate () {
+
+    // }
+
+
+
+    // var time = childSnapshot.val().firstTrain;
+    // var timeFormat = moment (time , "hhmmss").format("hhmm");
+    // console.log(timeFormat);
+    // var current = moment().format("hhmm");
+    // var differ = moment(time , "hhmmss").diff(moment(), 'minutes');
+    // console.log(differ);
+
+
+
  
   
-  // full list of items to the well
-  $("#full-member-list").append("<div class='well'><span class='member-name'> " + counter + "." +  childSnapshot.val().name +
-    " </span><span class='member-destination'> " + childSnapshot.val().destination +
-      " </span><span class='member-first-train'> " + childSnapshot.val().firstTrain +
-        " </span><span class='member-frequency'> " + childSnapshot.val().frequency + " </span></div>");
-        
   // Handle the errors
 }, function(errorObject) {
   console.log("Errors handled: " + errorObject.code);
@@ -65,11 +92,45 @@ dataRef.ref().on("child_added", function(childSnapshot) {
     
 dataRef.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
   // Change the HTML to reflect
-  $("#name-display").text(snapshot.val().name);
+  $("#train-name-display").text(snapshot.val().name);
   $("#destination-display").text(snapshot.val().destination);
-  $("#first-train-display").text(snapshot.val().firstTrain);
+//   $("#first-train-display").text(snapshot.val().firstTrain);
   $("#frequency-display").text(snapshot.val().frequency);
 });
 
 
+function nextTrainUpdate (startTime , frequ){
+    console.log("frequ   "+frequ)
+    var nextTrain = startTime;
+    console.log("next train " + nextTrain);
+    var condition = moment(nextTrain, "hh:mm").isBefore(moment());
+    console.log("condition " + condition);
+    // while (condition) {
+        var nextTrain1 = moment(nextTrain, 'hh:mm').add(frequ, 'm');
+        console.log("inside the wile loop");
+        condition = moment(nextTrain, "hh:mm").isBefore(moment());  
+        console.log("condition " + condition);
+    // }
+    console.log("nextTrain1  "  +  nextTrain1); 
+    return nextTrain;
+}
+
+// function that calculates how many minutes away is the train which schedueled for the arrival at time t 
+function MinsAwayUpdate ( t ) {   
+    // if next arival time is after current time, calculate the time difference and return the value
+    if (moment(t, "hh:mm").isAfter(moment())){
+        var minAway = moment(t, "hh:mm").diff(moment(), "minutes");
+        console.log("nextTrain comes in " + minAway + " minutes");
+        return minAway;
+    } 
+    // if time t == right now return 0 meaning train is here right now
+    else if(moment(t, "hh:mm").isSame(moment())){
+        console.log("Train is here");
+        return "is here right now";
+    } // otherwise return the string "already departed"
+    else {
+        console.log('OOOPS! This train has already departed , checkout the future arrivals');
+        return "already departed";
+    }
+}
 
